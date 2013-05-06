c179ad2ff8f957fde2fbb53addabb9e44005b0c2
Mathias Brossard <mathias@brossard.org>
2013-05-06 04:35:55 +0200

    Improve tests and add some asserts

--- tests/heavy.c
+++ tests/heavy.c
@@ -37,7 +37,7 @@ void dummy_task(void *arg) {
 
 int main(int argc, char **argv)
 {
-    int i;
+    int i, copy = 1;
 
     left = SIZE;
     pthread_mutex_init(&lock, NULL);
@@ -54,12 +54,15 @@ int main(int argc, char **argv)
         assert(threadpool_add(pool[0], &dummy_task, &(tasks[i]), 0) == 0);
     }
 
-    while(left > 0) {
+    while(copy > 0) {
         usleep(10);
+        pthread_mutex_lock(&lock);
+        copy = left;
+        pthread_mutex_unlock(&lock);
     }
 
     for(i = 0; i < QUEUES; i++) {
-        threadpool_destroy(pool[i], 0);
+        assert(threadpool_destroy(pool[i], 0) == 0);
     }
 
     pthread_mutex_destroy(&lock);
--- tests/thrdtest.c
+++ tests/thrdtest.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "threadpool.h"
 
@@ -11,7 +12,7 @@ int tasks = 0, done = 0;
 pthread_mutex_t lock;
 
 void dummy_task(void *arg) {
-    usleep(1000);
+    usleep(10000);
     pthread_mutex_lock(&lock);
     done++;
     pthread_mutex_unlock(&lock);
@@ -23,7 +24,7 @@ int main(int argc, char **argv)
 
     pthread_mutex_init(&lock, NULL);
 
-    pool = threadpool_create(THREAD, QUEUE, 0);
+    assert((pool = threadpool_create(THREAD, QUEUE, 0)) != NULL);
     fprintf(stderr, "Pool started with %d threads and "
             "queue size of %d\n", THREAD, QUEUE);
 
@@ -32,13 +33,13 @@ int main(int argc, char **argv)
         tasks++;
         pthread_mutex_unlock(&lock);
     }
+
     fprintf(stderr, "Added %d tasks\n", tasks);
 
-    while(tasks / 2 > done) {
-        sleep(1);
+    while((tasks / 2) > done) {
+        usleep(10000);
     }
-    fprintf(stderr, "Did %d tasks before shutdown\n", done);
-    threadpool_destroy(pool, 0);
+    assert(threadpool_destroy(pool, 0) == 0);
     fprintf(stderr, "Did %d tasks\n", done);
 
     return 0;
